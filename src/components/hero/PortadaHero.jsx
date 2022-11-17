b8102397ed0180d4303c57123e6aa1a85f6fb051
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2022-11-17 17:44:31 -0500

    first commit

---
+++ src/components/hero/PortadaHero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const PortadaHero = () => {
+    return (
+        <div>
+            <div className='grid-portada-hero'>
+                <div className='ph-left'>
+                    <h1>
+                        EXPERIMENTA LO<br/>
+                        MEJOR DE LOS <br/>
+                        ESPORTS
+                    </h1>
+                    <p>
+                        Compite con los mejores jugadores del mundo,<br/>
+                        crea tu comunidad y prepárate para participar<br/>
+                        en torneos de clase mundial 
+                    </p>
+                    <button className='button-ph'>¡Únete ahora!</button>
+                </div>
+                <div className="ph-right">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PortadaHero
