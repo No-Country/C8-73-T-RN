8459de3b9abf1499f6e3b674a237950487fc3928
davidmedev <davidcatrielmamaniescalera@gmail.com>
2022-11-28 14:18:53 -0300

    created error page component

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,8 +1,16 @@
 const Error = () => {
     return (
-        <>
-            <main>contenido</main>
-        </>
+        <main>
+            <section className="error">
+                <picture className="error-picture">
+                    <img src="" alt="" />
+                </picture>
+                <h2 className="error-title">Ops... parece que tu juego colapso</h2>
+                <p className="error-paragraph">
+                    No te preocupes, haz clic <strong className="error-strong">aquí</strong> para buscar otros juegos
+                </p>
+            </section>
+        </main>
     );
 };
 
