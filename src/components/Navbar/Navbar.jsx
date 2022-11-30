da81ade559237b3dc7dd5e4fe09c631cdddc553a
davidmedev <davidcatrielmamaniescalera@gmail.com>
2022-11-29 21:42:37 -0300

    added event to open login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import { LoginContext } from "../../context/login/LoginContext"; // CONTEXTO
+import { useContext } from "react"; // HOOK
 
 const Navbar = () => {
+    const { updateOnLoginWithOption } = useContext(LoginContext); // AYUDANTES
+
+    const handleOnLoginWithOption = () => updateOnLoginWithOption(true); // EVENTO
+
     return (
         <nav>
-        <div className="title">
-            <span>e/ stadium</span>
-        </div>
-            <ul class="navigation">
-                <li> <a href="#">Home</a></li>
-                <li> <a href="#"> Quienes Somos</a></li>
-                <li> <a href="#"> Juegos</a></li>
-                <li> <a href="#"> Torneo</a></li>
-                <div class="buttons">
-                    <a class="btn-main" href="#"> Iniciar Sesión </a>
+            <div className="title">
+                <span>e/ stadium</span>
+            </div>
+            <ul className="navigation">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Quienes Somos</a>
+                </li>
+                <li>
+                    <a href="#">Juegos</a>
+                </li>
+                <li>
+                    <a href="#">Torneo</a>
+                </li>
+                <div className="buttons">
+                    <button onClick={handleOnLoginWithOption} className="btn-main">
+                        Iniciar Sesión
+                    </button>
                 </div>
             </ul>
-
-
         </nav>
-    )
-}
+    );
+};
 
-export {Navbar}
+export { Navbar };
