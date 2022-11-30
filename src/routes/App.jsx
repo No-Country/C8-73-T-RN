3023382dd939eeb9bc0db0fcf3b6c90df3319e63
davidmedev <davidcatrielmamaniescalera@gmail.com>
2022-11-30 20:37:50 -0300

    updated application routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,32 +1,32 @@
-import "../styles/App.scss"; // ESTILOS DE LA APP
-import { LoginContextProvider } from "../context/login/LoginContextProvider"; // CONTEXTO
-import { BrowserRouter, Route, Routes } from "react-router-dom"; // COMPONENTES ROUTER DOM
-import { Error } from "../pages/error/Error"; // COMPONENTE
-import { Inicio } from "../pages/inicio/Inicio"; // COMPONENTE
-import { Juegos } from "../pages/juegos/Juegos"; // COMPONENTE
-import { Torneos } from "../pages/torneos/Torneos"; // COMPONENTE
-import { Navbar } from "../components/navbar/Navbar"; // COMPONENTE
-import { LoginLayout } from "../pages/login/LoginLayout"; // COMPONENTE
+import '../styles/App.scss'; // ESTILOS DE LA APP
+import { LoginContextProvider } from '../context/login/LoginContextProvider'; // CONTEXTO
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; // COMPONENTES ROUTER DOM
+import { Layout } from '../pages/layout/Layout'; // COMPONENTE
+import { Error } from '../pages/error/Error'; // COMPONENTE
+import { Inicio } from '../pages/inicio/Inicio'; // COMPONENTE
+import { Juegos } from '../pages/juegos/Juegos'; // COMPONENTE
+import { Torneos } from '../pages/torneos/Torneos'; // COMPONENTE
+import { LayoutLogin } from '../pages/login/LayoutLogin'; // COMPONENTE
+import { LoginForm } from '../components/form/LoginForm'; // COMPONENTE
+import { RegisterForm } from '../components/form/RegisterForm'; // COMPONENTE
 
 const App = () => {
     return (
         <LoginContextProvider>
-            <header>
-                <Navbar />
-            </header>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Inicio />} />
-                    <Route path="/*" element={<Error />} />
-                    <Route path="/juegos" element={<Juegos />} />
-                    <Route path="/torneos" element={<Torneos />} />
-                    <Route path="/login" element={<LoginLayout />}>
-                        <Route index element={<h1>form de inicio</h1>} />
-                        <Route path="registro" element={<h1>form de registro</h1>} />
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Inicio />} />
+                        <Route path="*" element={<Error />} />
+                        <Route path="juegos" element={<Juegos />} />
+                        <Route path="torneos" element={<Torneos />} />
+                        <Route path="login/" element={<LayoutLogin />}>
+                            <Route index element={<LoginForm />} />
+                            <Route path="registro" element={<RegisterForm />} />
+                        </Route>
                     </Route>
                 </Routes>
             </BrowserRouter>
-            <footer>footer</footer>
         </LoginContextProvider>
     );
 };
