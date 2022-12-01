39f57c1044f6ec6865ff7f9e0229b1a4e0fe1391
CariBosio <caribosio72gmail.com>
2022-11-30 21:06:13 -0300

    Crear Torneo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import "./styles/App.scss"; // ESTILOS
 import Footer from "./components/footer/footer";
 import Newsletter from "./components/newsletter/newsletter";
+import CrearTorneo from "./components/crearTorneo/crearTorneo";
 
 function App() {
   return (
     <div className="App">
       <Newsletter />
       <Footer />
-    </div>
+   </div>
   );
 }
 
