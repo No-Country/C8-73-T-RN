2280e44528bdde70f4e0a864178c039ded2a2a75
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2022-12-08 17:42:23 -0500

    restore

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { LoginContext } from "../../context/login/LoginContext"; // CONTEXTO
 import { Outlet } from "react-router-dom"; // COMPONENTE REACT ROUTER
 import { LoginWithOption } from "../../components/login/LoginWithOption"; // COMPONENTE
-import  {Navbar}  from "../../components/Navbar/Navbar"; // COMPONENTE
+import  {Navbar}  from "../../components/navbar/Navbar"; // COMPONENTE
 import { useContext } from "react"; // HOOKS
 import {Footer} from "../../components/footer/Footer";
 import PortadaHero from "../../components/hero/PortadaHero";
