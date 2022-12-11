9fbab53e498d4905cf1dd4b3eca72ad3efda8d6c
davidmedev <davidmedev@gmail.com>
2022-12-11 14:58:25 -0300

    added tournaments component styles

--- src/pages/torneos/Torneos.jsx
+++ src/pages/torneos/Torneos.jsx
@@ -1,9 +1,17 @@
-import TournamentTable from "../../components/tournamentTable/TournamentTable";
+import TournamentTable from '../../components/tournamentTable/TournamentTable'; // COMPONENTE
+import { ProgrammedTournaments } from '../../components/account/ProgrammedTournaments'; // COMPONENTE
 
 const Torneos = () => {
     return (
         <main>
+            {/* COMPONENTE */}
             <TournamentTable />
+            <section className="tournaments-tourney">
+                {/* COMPONENTE */}
+                <ProgrammedTournaments title="Próximos torneos" search={true} filter={true} />
+                {/* COMPONENTE */}
+                <ProgrammedTournaments title="Torneos finalizados" link={true} />
+            </section>
         </main>
     );
 };
